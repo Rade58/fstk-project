8f9eee7934a91ebeb9448ab2492235b744343e5a
rade <bajic.rade2@gmail.com>
2025-10-27 15:59:29 +0100

    initial

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,17 @@
+import { dirname } from "node:path";
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  // added this in case we are having multiple projects in one repo
+  // We don't have multiple projects but it's good to be explicit
+  // We would use this in turborepo monorepos especially
+  turbopack: {
+    root: dirname(__filename),
+  },
+  // ------------------------------------------------------
 };
 
 export default nextConfig;
